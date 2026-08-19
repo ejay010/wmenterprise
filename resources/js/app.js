decdd1b03e0165e03bf38fb2d5f46aa4430c290b
ejay010 <ejay010@gmail.com>
2026-08-19 12:24:26 -0400

    updated vite asset loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-// Instructs Vites to bundle all images in this directory
+// Instructs Vite to bundle all images in the resources/images directory into the manifest
 import.meta.glob([
     '../images/**',
-]);
+], { eager: true });
